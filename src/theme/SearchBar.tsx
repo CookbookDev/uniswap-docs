78467d80e40e436259268045181e965ae5a34c7e
Ae0h <dontlisten33@gmail.com>
2024-08-15 18:04:06 -0700

    feat: Cookbook Onboard integration

--- src/theme/SearchBar.tsx
+++ src/theme/SearchBar.tsx
@@ -1,7 +1,13 @@
+import React from 'react'
+import AskCookbook from '@cookbookdev/docsbot/react'
+import BrowserOnly from '@docusaurus/BrowserOnly'
 import OriginalSearchBar from '@theme-original/SearchBar'
 import { TraceEvent } from '@uniswap/analytics'
 import { BrowserEvent, SharedEventName } from '@uniswap/analytics-events'
-import React from 'react'
+
+/** It's a public API key, so it's safe to expose it here */
+const COOKBOOK_PUBLIC_API_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NWMxOGQyNTA1MjA1MDZmZmEwMDhjMzYiLCJpYXQiOjE3MDcxODMzOTcsImV4cCI6MjAyMjc1OTM5N30.j1gfmvmmfz_cLwAheAI3_mrGiseW_KnGm_MQ1_VbeSY'
 
 export default function SearchBarWithAnalytics(props) {
   return (
@@ -12,6 +18,7 @@ export default function SearchBarWithAnalytics(props) {
           <OriginalSearchBar {...props} />
         </div>
       </TraceEvent>
+      <BrowserOnly>{() => <AskCookbook apiKey={COOKBOOK_PUBLIC_API_KEY} /> }</BrowserOnly>
     </>
   )
 }
